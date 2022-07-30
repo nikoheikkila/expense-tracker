34de8554b4c6701625adb672b4ff770a35b934b2
Niko Heikkilä <niko.heikkila@futurice.com>
2022-07-30 14:40:53 +0300

    refactor: do not inject any Fastify options by default

--- backend/src/api/client.ts
+++ backend/src/api/client.ts
@@ -1,14 +1,8 @@
 import fastify, { FastifyInstance, FastifyServerOptions } from 'fastify';
 import * as Routes from './routes';
 
-const defaultServerOptions: FastifyServerOptions = {
-	logger: {
-		level: 'info',
-	},
-};
-
 const APIClient = (options: FastifyServerOptions = {}): FastifyInstance => {
-	return Routes.register(fastify({ ...defaultServerOptions, ...options }));
+	return Routes.register(fastify(options));
 };
 
 export default APIClient;
