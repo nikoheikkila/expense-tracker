83c7d4a50383c9c99069e0427129dc246ba04861
Niko Heikkilä <niko.heikkila@futurice.com>
2022-09-24 20:10:10 +0300

    feat: add Home component

---
+++ frontend/src/views/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+	const [expenses, setExpenses] = React.useState([]);
+
+	return (
+		<main>
+			<h1>Expenses for this month</h1>
+		</main>
+	);
+};
+
+export default Home;
