47dc9bfd1583bbcb198cac5e5e81fa33adf66013
Niko Heikkilä <niko.heikkila@futurice.com>
2022-09-24 18:44:29 +0300

    feat: setup testing for frontend

---
+++ frontend/tests/App.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { describe, test } from 'vitest';
+
+import App from '../src/App';
+
+describe('App', () => {
+	test('renders', () => {
+		render(<App />);
+
+		expect(screen.getByText('Vite + React')).toBeVisible();
+	});
+});
